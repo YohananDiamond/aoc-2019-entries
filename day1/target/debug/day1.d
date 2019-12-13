dd677690fbc27df5aaebcaf84c8e12f23c5870af
YohananDiamond <mitonanan12@gmail.com>
2019-12-12 22:53:00 -0300

    Removed target folders
    As a complement for last commit, I've removed the remaining "target"
    folders that, for some reason, weren't removed with the code I put on
    .gitignore (I thought it would do that for me)
    
    I've also tested here and new "target" directories aren't tracked by
    git, so I think this problem is over.

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/yohanan/git/aoc-2019/day1/target/debug/day1: /home/yohanan/git/aoc-2019/day1/src/main.rs
--- day1/target/debug/deps/day1-26c35668dc2a89ea.d
+++
@@ -1,5 +0,0 @@
-/home/yohanan/git/aoc-2019/day1/target/debug/deps/day1-26c35668dc2a89ea: src/main.rs
-
-/home/yohanan/git/aoc-2019/day1/target/debug/deps/day1-26c35668dc2a89ea.d: src/main.rs
-
-src/main.rs:
