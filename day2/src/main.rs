615e84b2b49a51eaf4f2c6a3a6c74f6b81c8e51e
YohananDiamond <mitonanan12@gmail.com>
2020-01-06 19:27:01 -0300

    Cleaned up code from day1 & day2, added .gitignore

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,6 +4,7 @@
 
 fn main() {
     let puzzle_input = vec![1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,5,23,1,23,9,27,2,27,6,31,1,31,6,35,2,35,9,39,1,6,39,43,2,10,43,47,1,47,9,51,1,51,6,55,1,55,6,59,2,59,10,63,1,6,63,67,2,6,67,71,1,71,5,75,2,13,75,79,1,10,79,83,1,5,83,87,2,87,10,91,1,5,91,95,2,95,6,99,1,99,6,103,2,103,6,107,2,107,9,111,1,111,5,115,1,115,6,119,2,6,119,123,1,5,123,127,1,127,13,131,1,2,131,135,1,135,10,0,99,2,14,0,0];
+
     part_1(&puzzle_input);
     // part_2();
 }
@@ -14,12 +15,10 @@ fn part_1(puzzle_input: &Vec<i32>) {
 }
 
 fn compute(input_memory: &Vec<i32>) -> Vec<i32> {
-
     let mut memory = input_memory.to_vec();
     let mut running = true;
 
     for slice_base_index in 0 .. (memory.len() as f64 / 4f64).ceil() as i32 {
-
         // Set up command info
         let command_position = slice_base_index * 4;
         let arg1 = (command_position + 1) as usize;
@@ -29,7 +28,6 @@ fn compute(input_memory: &Vec<i32>) -> Vec<i32> {
 
         // Parse and execute commands
         match memory[command_position] {
-
             1 => {
                 // Case 1: sum
                 // Only access the contents if needed (reduces chances of out-of-bound index error)
@@ -59,6 +57,7 @@ fn compute(input_memory: &Vec<i32>) -> Vec<i32> {
                 );
                 memory[dest_contents] = memory[arg1_contents] * memory[arg2_contents];
             },
+
             99 => {
                 println!("Halt!");
                 running = false;
@@ -73,7 +72,6 @@ fn compute(input_memory: &Vec<i32>) -> Vec<i32> {
     }
 
     memory
-
 }
 
 fn part_2() {
